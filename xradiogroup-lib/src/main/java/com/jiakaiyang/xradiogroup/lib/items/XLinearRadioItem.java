0230509fbc0aeb2675bd752cc4010c071c0f5651
jiakaiyang <kaiyangjia@163.com>
2017-09-08 14:41:59 +0800

    1. XLinerRadioItem add attrs checked and fixed.

--- xradiogroup-lib/src/main/java/com/jiakaiyang/xradiogroup/lib/items/XLinearRadioItem.java
+++ xradiogroup-lib/src/main/java/com/jiakaiyang/xradiogroup/lib/items/XLinearRadioItem.java
@@ -1,10 +1,13 @@
 package com.jiakaiyang.xradiogroup.lib.items;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.SoundEffectConstants;
 import android.widget.LinearLayout;
 
+import com.jiakaiyang.xradiogroup.lib.R;
 import com.jiakaiyang.xradiogroup.lib.XRadioItem;
 
 /**
@@ -14,19 +17,60 @@ import com.jiakaiyang.xradiogroup.lib.XRadioItem;
  */
 
 public class XLinearRadioItem extends LinearLayout implements XRadioItem {
+    private static final int[] CHECKED_STATE_SET = {
+            android.R.attr.state_checked
+    };
+
+
     private boolean fixed;
     private boolean checked;
 
+
+    private XRadioItem.OnCheckedChangeListener onCheckedChangeListener;
+
     public XLinearRadioItem(Context context) {
-        super(context);
+        this(context, null, 0);
     }
 
     public XLinearRadioItem(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public XLinearRadioItem(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        TypedArray a = context.obtainStyledAttributes(
+                attrs, R.styleable.XLinearRadioItem, defStyleAttr, 0);
+        fixed = a.getBoolean(R.styleable.XLinearRadioItem_fixed, false);
+        checked = a.getBoolean(R.styleable.XLinearRadioItem_checked, false);
+        a.recycle();
+    }
+
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        }
+        return drawableState;
+    }
+
+    @Override
+    public boolean performClick() {
+        toggle();
+
+        final boolean handled = super.performClick();
+        if (!handled) {
+            // View only makes a sound effect if the onClickListener was
+            // called, so we'll need to make one here instead.
+            playSoundEffect(SoundEffectConstants.CLICK);
+        }
+
+        return handled;
     }
 
     @Override
@@ -41,12 +85,14 @@ public class XLinearRadioItem extends LinearLayout implements XRadioItem {
 
     @Override
     public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
-
+        this.onCheckedChangeListener = listener;
     }
 
     @Override
     public void setChecked(boolean checked) {
         this.checked = checked;
+
+        refreshDrawableState();
     }
 
     @Override
@@ -56,6 +102,6 @@ public class XLinearRadioItem extends LinearLayout implements XRadioItem {
 
     @Override
     public void toggle() {
-
+        setChecked(!checked);
     }
 }
